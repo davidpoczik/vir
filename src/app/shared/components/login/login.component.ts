f728cc19f9a655a85fb19a8284bf94aa473468b8
poczikdavid <pdavid.allas@gmail.com>
2022-02-03 13:45:42 +0100

    login design

--- src/app/shared/components/login/login.component.ts
+++ src/app/shared/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HotToastService } from '@ngneat/hot-toast';
+import { tap } from 'rxjs';
 import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
@@ -9,27 +11,54 @@ import { AuthService } from 'src/app/core/services/auth.service';
 })
 export class LoginComponent implements OnInit {
 
-  inputUsername = "simonz"
-  inputPassword = "z4r4k1k3np4ch1"
+  showPassword = false
+  showEyes = true
+  submitted = false
 
-  onSubmit() {
-    this.AuthService.login({
-      username: this.inputUsername,
-      password: this.inputPassword
-    }).pipe(
+  loginForm: FormGroup
+
+  onSubmit(form: FormGroup) {
+    this.submitted = true
+
+    this.AuthService.login(form.value).pipe(
       this.toastService.observe({
         loading: 'Egy pillanat...',
         success: 'success',
         error: 'error'
       })
-    ).subscribe((response) => {
-      if (response.success) {
-        this.router.navigate(['/modules'])
-      }
+    )
+      .subscribe((response) => {
+        console.log(response)
+        if (response.success) {
+          this.router.navigate(['/modules'])
+        } else {
+          this.submitted = false
+        }
+      }, error => {
+        this.submitted = false
+      })
+  }
+
+  onShowPassword() {
+    this.showPassword = !this.showPassword
+  }
+
+  ngOnInit(): void {
+    this.loginForm.controls['password'].valueChanges.subscribe((r) => {
+      console.log(r)
+      this.showEyes = r.length > 0
     })
   }
 
-  ngOnInit(): void { }
+  constructor(
+    private AuthService: AuthService,
+    private router: Router,
+    private toastService: HotToastService) {
 
-  constructor(private AuthService: AuthService, private router: Router, private toastService: HotToastService) { }
+    this.loginForm = new FormGroup({
+      username: new FormControl('simonz'),
+      password: new FormControl('z4r4k1k3np4ch1')
+    })
+
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/login/login.component';
@@ -33,6 +33,7 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule,
     HotToastModule.forRoot({}),
     TranslateModule.forRoot({
@@ -46,6 +47,7 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
   exports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     HeaderComponent,
     FooterComponent,
     TranslateModule,
